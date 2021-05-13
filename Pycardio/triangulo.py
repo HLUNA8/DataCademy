d45ac1dbde0c4d1df7486581dff24f13747e2425
HLUNA <lunahuascar@gmail.com>
2021-05-12 23:44:36 -0400

    se termino el programa triangulo.py, se le agrego la funcion de determinar que tipo de triangulo era el ingresado

--- Pycardio/triangulo.py
+++ Pycardio/triangulo.py
@@ -1,5 +1,17 @@
 import math 
 
+def Clase_triangulo(Base, Lado1, Lado2):
+	Tipo_Triangulo = ''
+	
+	if Base == Lado1 and Base == Lado2 :
+		Tipo_Triangulo = 'Equilatero y todos los lados del triangulo son iguales'
+	elif Base != Lado1 and Base != Lado2 and Lado1 != Lado2 :
+		Tipo_Triangulo = 'Escaleno y todos los lados del triangulo son distintos'
+	else :
+		Tipo_Triangulo = 'Isoceles y dos de los lados de tu triangulo son iguales'
+
+	return Tipo_Triangulo
+
 def Area(Base,Altura):
 	return (Base*Altura)/2
 
@@ -13,5 +25,13 @@ def run():
 
 	print (f'El area de tu triangulo es {Area_triangulo} unidades cuadradas')
 
+	print ("Quires saber de que tipo es tu triangulo? Ingresa los dos lados restantes de tu triangulo!")
+
+	Lado1 = float(input("Ingresa uno de los lados faltantes: "))
+	Lado2 = float(input("Ingresa el otro lado faltante: "))
+
+	Tipo_triangulo = Clase_triangulo(Base, Lado1, Lado2)
+
+	print(f'Tu triangulo es un {Tipo_triangulo}  :)')
 if __name__ == "__main__" :
 	run()
