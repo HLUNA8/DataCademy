14a2b100284b0a485a0ec3ab31ee925b4553b355
HLUNA <lunahuascar@gmail.com>
2021-05-12 23:02:01 -0400

    se agrego la funcion de calcular el area de un triangulo al programa triangulo.py

--- Pycardio/triangulo.py
+++ Pycardio/triangulo.py
@@ -1,8 +1,17 @@
 import math 
 
+def Area(Base,Altura):
+	return (Base*Altura)/2
 
-def run:
-	print("Hola, ingresa los valores del triangulo")
+def run():
+	print("Hola, quieres calcular el area de tu triangulo? ingresa su base y su altura")
 
-if main == 'name' :
-run 
+	Base = float(input("base: "))
+	Altura = float(input("Altura: "))
+
+	Area_triangulo = Area(Base,Altura)
+
+	print (f'El area de tu triangulo es {Area_triangulo} unidades cuadradas')
+
+if __name__ == "__main__" :
+	run()
