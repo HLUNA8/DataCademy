4c29118724623ceac045859edd446b2053f3dee6
HLUNA <lunahuascar@gmail.com>
2021-05-13 11:58:40 -0400

    se termino el codigo del archivo yankenpo

--- Pycardio/yankenpo.py
+++ Pycardio/yankenpo.py
@@ -1,35 +1,122 @@
-import time
 import random
 
-def ganador(jugador_eleccion, compu_eleccion):
-	if jugador_eleccion 
+def descifrar(numero_descifrar):
+	descifrar = ''
+	if numero_descifrar == 1 :
+		descifrar = 'piedra'
+	elif numero_descifrar == 2 :
+		descifrar = 'papel'
+	else :
+		descifrar = 'tijeras'
 
-def elegir():
-	
-	posibilidades = ['piedra', 'papel', 'tijera']
-	eleccion = random.choice(posibilidades)
+	return descifrar
+
+def ganador_partida(jugador1, jugador2):
+	ganador = 0
+	if jugador1 == jugador2 :
+		ganador = 3
+
+	elif (jugador1 == 1 and jugador2 == 3) or (jugador1 == 2 and jugador2 == 1) or (jugador1 == 3 and jugador2 == 2):
+		ganador = 1
+
+ 
+	else :
+		ganador = 2
+
+	return ganador
+
+def eleccion(jugador):
+	print(f"{jugador} es tu turno elige:")
+	print("		1. piedra")
+	print("		2. papel")
+	print("		3. tijeras")
+
+	eleccion = int (input())
+
 	return eleccion
 
 def run():
+	print("Hola!, juguemos unas partidas de piedra papel ")
+	print("		1. jugar con la computadora")
+	print("		2. jugar con un amigo")
 
-	print("Hola!, jueguemos piedra, papel o tijeras")
+	modo_juego = int(input())
+	numero_partidas = int(input("cuantas partidas deseas jugar :"))
 
-	
-	jugadas_ganadas = 0
+	if modo_juego == 1:
+		num_juegos = 0
+		juegos_ganados = 0
+		juegos_perdidos = 0
 
-	for _ in range (3):
-		print("elige uno:")
-        	print("         1. piedra")
-        	print("         2. papel")
-       		print("         3. tijera")
+		while num_juegos < numero_partidas:
+			jugador = eleccion(jugador = 'jugador')
+			compu = random.randint(1,3)
+			ganador = ganador_partida(jugador, compu)
+			eleccion_jugador = descifrar(jugador)
+			eleccion_compu = descifrar(compu)
+
+
+			if ganador == 1:
+				juegos_ganados += 1
+				num_juegos += 1 
+				print (f"{eleccion_jugador} vs {eleccion_compu} tu ganas!")
+
+			elif ganador == 2:
+				juegos_perdidos += 1
+				num_juegos +=1
+				print(f"{eleccion_jugador} vs {eleccion_compu} perdiste :(")
+
+			else :
+				print(f"{eleccion_jugador} vs {eleccion_compu} es un empate!!")
+
+		if juegos_ganados > juegos_perdidos :
+			print("Ganaste!!")
+
+		elif juegos_ganados == juegos_perdidos :
+			print("Oooo !! Empatamos, fue una buena ronda de juegos  :)")
+
+		else:
+			print(f"Perdiste {juegos_perdidos} a {juegos_ganados} :(  suerte la proxima!!")
+
+
+
+	if modo_juego == 2 :
+                num_juegos = 0
+                juegos_ganados = 0
+                juegos_perdidos = 0
+
+                while num_juegos < numero_partidas:
+
+                        jugador1 = eleccion(jugador = 'jugador1')
+                        jugador2 = eleccion(jugador = 'jugador2')
+                        ganador = ganador_partida(jugador1, jugador2)
+                        eleccion_jugador1 = descifrar(jugador1)
+                        eleccion_jugador2 = descifrar(jugador2)
+
+
+                        if ganador == 1:
+                                juegos_ganados += 1
+                                num_juegos += 1
+                                print (f"{eleccion_jugador1} vs {eleccion_jugador2}, jugador1 tu ganaste!!")
+
+                        elif ganador == 2:
+                                juegos_perdidos += 1
+                                num_juegos +=1
+                                print(f"{eleccion_jugador1} vs {eleccion_jugador2}, jugador2 tu ganaste")
+
+                        else :
+                                print(f"{eleccion_jugador} vs {eleccion_compu} es un empate!!")
+
+                if juegos_ganados > juegos_perdidos :
+                        print("Ganaste jugador1!!")
+
+                elif juegos_ganados == juegos_perdidos :
+                        print("Oooo !! Empataron {juegos_ganados} a {juegos_perdidos}, fue una buena ronda de juegos  :)")
+
+                else:
+                        print(f"Ganaste jugador2!!")
 		
-		jugador_eleccion = int(input())
 
-		compu_eleccion = elegir()
-		gandor_jugador = ganador(jugador_eleccion, compu_eleccion)
-		if ganador_jugador == true :
-			jugadas_ganadas += 1
-	
 
 if __name__ == "__main__":
 	run()
