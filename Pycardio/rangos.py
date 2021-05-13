a097f7eaa711d3d6bd52e3d67c52ab5084954fa6
HLUNA <lunahuascar@gmail.com>
2021-05-13 09:53:39 -0400

    se agrego el archivo de rangos y verifica si un numero esta dentro de un rango de valores, aun hay problemas con los print

---
+++ Pycardio/rangos.py
@@ -0,0 +1,30 @@
+def verificar(lim_inferior, lim_superior, numero):
+	resultado = 0
+	if numero > lim_inferior and numero < lim_superior:
+		resultado = 1
+	return resultado
+
+def run():
+	print("Hey, ingresa tres valores, un limite inferior, un limite superior y un valor intermedio")
+	
+	lim_inferior = int(input("limite inferior: "))
+	lim_superior = int(input("limite superior: "))
+	
+	resultado = 0	
+
+	numero = int(input("numero: "))
+
+	resultado  = verificar(lim_inferior, lim_superior, numero)
+
+	if resultado == 1:
+		print(f"el numero {numero} esta dentro del limete entre {lim_inferior} y {lim_superior} ")
+
+	else :
+		while resultado == 0 :
+			print(f"el anterior numero no se encontraba entre los limites {lim_inferior} y {lim_superior}")
+			numero = int (input("ingresa otro numero :"))
+			resultado = verificar(lim_inferior, lim_superior, numero)
+		print("el numero {numero} es correcto esta entre los limites {lim_inferior} y {lim_superior}")
+
+if __name__ == "__main__":
+	run()
